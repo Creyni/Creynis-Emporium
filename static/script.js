8b1b0c5e18a8f702a83156f56a02d768fb545b6b
Creyni <38184376+Creyni@users.noreply.github.com>
2021-09-25 23:10:21 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -551,10 +551,10 @@ function toggleCols() {
     });
 }
 function getWorksheetUrl(spreadsheetId, worksheetId) {
-    return "https://spreadsheets.google.com/feeds/list/" + spreadsheetId + "/" + worksheetId + "/public/values?alt=json";
+    return "https://googleapis.com/auth/spreadsheets.readonly" + spreadsheetId + "/" + worksheetId + "/public/values?alt=json";
 }
 function getSpreadsheetUrl(spreadsheetId) {
-    return "https://spreadsheets.google.com/feeds/worksheets/" + spreadsheetId + "/public/basic?alt=json";
+    return "https://googleapis.com/auth/spreadsheets.readonly" + spreadsheetId + "/public/basic?alt=json";
 }
 function getValue(field) {
     if (field && field.$t) return field.$t;
